f3c08d7fe5a0d35ba7152b6c75bc5507f0354865
Alexander Brown <DrJKL0424@gmail.com>
2023-08-01 10:33:52 -0700

    App shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <CssBaseline />
+      <div className="grid grid-cols-1 grid-rows-[auto_1fr] gap-4">
+        <AppBar classes="grid">
+          <Toolbar className="flex flex-auto justify-between">
+            <Typography className="flex-auto" variant="h5" color="initial">
+              Prompt Crafter
+            </Typography>
+            <Typography className="flex-auto" variant="h6" color="initial">
+              v0.0.1
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <main className="p-4">Main stuff</main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { BrowserRouter } from 'react-router-dom';
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: red[500],
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
