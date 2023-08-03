8ac92a218bb9c76b605721624334d8913fc20adb
Alexander Brown <DrJKL0424@gmail.com>
2023-08-03 01:58:34 -0700

    Use react-spaces for layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState } from 'react';
 import { useMonaco } from '@monaco-editor/react';
 import { conf, language } from './common/sdprompt_monaco';
 import monaco from 'monaco-editor';
+import { Fill, LeftResizable, Top, ViewPort } from 'react-spaces';
+
 function App() {
   const [promptText, setPromptText] = useState(
     localStorage.getItem('pc.active_prompt') ?? '',
@@ -41,41 +43,42 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <div className="h-full grid grid-cols-1 grid-rows-[auto_minmax(0,_1fr)]">
-        <AppBar>
-          <Toolbar className="flex flex-auto justify-between">
-            <Typography
-              className="flex-auto select-none"
-              variant="h5"
-              color="initial">
-              Prompt Crafter
-            </Typography>
-            <Typography
-              className="flex-auto text-right select-none"
-              variant="body1"
-              color="initial">
-              v0.0.1
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-        <main className="grid grid-cols-2 grid-rows-1 p-4 gap-4">
-          <Paper elevation={16}>
+      <ViewPort>
+        <Top size={64}>
+          <AppBar position="static">
+            <Toolbar className="flex flex-auto justify-between">
+              <Typography
+                className="flex-auto select-none"
+                variant="h5"
+                color="initial">
+                Prompt Crafter
+              </Typography>
+              <Typography
+                className="flex-auto text-right select-none"
+                variant="body1"
+                color="initial">
+                v0.0.1
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Top>
+        <Fill className="p-4">
+          <LeftResizable size="50%">
             <Editor
               onMount={handleEditorDidMount}
               defaultValue={promptText}
               onChange={handleEditorTextChange}
             />
-          </Paper>
-          <Paper elevation={16}>
+          </LeftResizable>
+          <Fill>
             <div className="overflow-y-auto h-full whitespace-pre-wrap p-4">
               {splitOnCommas(promptText).map((segment, idx) => (
                 <div key={`segment-${idx}`}>{segment}</div>
               ))}
             </div>
-          </Paper>
-        </main>
-      </div>
+          </Fill>
+        </Fill>
+      </ViewPort>
     </>
   );
 }
