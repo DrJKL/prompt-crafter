634397c4fc7f1b66af9c9deabaea47806792d2eb
Alexander Brown <DrJKL0424@gmail.com>
2023-08-02 20:13:19 -0700

    Going to be honest, I don't know what is causing the default export to not work.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), monacoEditorPlugin({})],
+  plugins: [react(), monacoEditorPlugin.default({})],
 });
