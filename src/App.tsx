e7706d745aae7d5ff9f816bc1bdcb02a8dc8512b
Alexander Brown <DrJKL0424@gmail.com>
2023-08-03 22:58:46 -0700

    Add some more sections for pending content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import { useEffect, useRef, useState } from 'react';
 import { useMonaco } from '@monaco-editor/react';
 import { conf, language } from './common/sdprompt_monaco';
 import monaco from 'monaco-editor';
-import { Fill, LeftResizable, Top, ViewPort } from 'react-spaces';
+import {
+  Bottom,
+  BottomResizable,
+  CenterType,
+  Fill,
+  LeftResizable,
+  Top,
+  ViewPort,
+} from 'react-spaces';
 
 function App() {
   const [promptText, setPromptText] = useState(
@@ -50,20 +58,22 @@ function App() {
               <Typography
                 className="flex-auto select-none"
                 variant="h5"
-                color="initial">
+                component="h1">
                 Prompt Crafter
               </Typography>
               <Typography
                 className="flex-auto text-right select-none"
-                variant="body1"
-                color="initial">
+                variant="subtitle1">
                 v0.0.1
               </Typography>
             </Toolbar>
           </AppBar>
         </Top>
-        <Fill className="p-4">
+        <Fill className="p-4 pb-10">
           <LeftResizable size="50%">
+            <Typography variant="h6" className="pl-6">
+              Editor
+            </Typography>
             <Editor
               onMount={handleEditorDidMount}
               defaultValue={promptText}
@@ -71,13 +81,28 @@ function App() {
             />
           </LeftResizable>
           <Fill>
-            <div className="overflow-y-auto h-full whitespace-pre-wrap p-4">
-              {splitOnCommas(promptText).map((segment, idx) => (
-                <div key={`segment-${idx}`}>{segment}</div>
-              ))}
-            </div>
+            <Fill>
+              <Typography variant="h6" className="pl-6">
+                Parsed
+              </Typography>
+              <div className="overflow-y-auto h-full whitespace-pre-wrap p-4">
+                {splitOnCommas(promptText).map((segment, idx) => (
+                  <div key={`segment-${idx}`}>{segment}</div>
+                ))}
+              </div>
+            </Fill>
+            <BottomResizable
+              size="20%"
+              className="bg-blue-600 p-2 text-xl font-mono">
+              <Typography variant="h6" component="h2" className="pl-6">
+                Saved
+              </Typography>
+            </BottomResizable>
           </Fill>
         </Fill>
+        <Bottom size={'2rem'} centerContent={CenterType.HorizontalVertical}>
+          Footer
+        </Bottom>
       </ViewPort>
     </>
   );
