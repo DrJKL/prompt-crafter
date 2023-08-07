7285e13319083352ce1d77f2b31168635d42a85b
Alexander Brown <DrJKL0424@gmail.com>
2023-08-06 20:19:30 -0700

    Better wrapping

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,8 +7,8 @@ const DEFAULT_OPTIONS: EditorProps = {
     padding: { top: 16 },
     minimap: { enabled: false },
     automaticLayout: true,
-    scrollBeyondLastLine: false,
-    wordWrap: 'wordWrapColumn',
+    // scrollBeyondLastLine: false,
+    wordWrap: 'on',
   },
   language: 'sdprompt',
 };
