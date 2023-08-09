a2f003308be91ac53d84192166aa96a61f9d8a7c
Alexander Brown <DrJKL0424@gmail.com>
2023-08-09 01:58:11 -0700

    Parser cleanup

--- src/common/sdprompt.ne
+++ src/common/sdprompt.ne
@@ -3,40 +3,59 @@
     // eslint-disable
     // @ts-nocheck
     import {basicPromptLexer} from './sdprompt_lexer';
-    const tag = (key: string) => (data: any[]) => [key, ...data.flat()]; 
-    const unwrap = (data: any[]) => data[0][0];
     const DEFAULT_BOUND = {
         min: "1",
         max: "1",
     };
+    
+    const tag = (key: string) => (data: any[]) => [key, ...data.flat()]; 
+    const unwrap = (data: any[]) => data[0][0];
+    
+    function wrapVariants(data: any[]) {
+        return {
+            type: 'variants',
+            bound: data[1] ?? DEFAULT_BOUND,
+            variants: data[2],
+        };
+    }
+    function flattenVariantsList(data: any[]) {
+        return [data[0][0], ...data[1]];
+    }
+    function lexerValue(data: any[]) {
+        return data[0].value;
+    }
+    function constructLiteral(data: any[]) {
+        return {
+            type: 'literal',
+            value: data[0].value,
+        }
+    }
+    function constructBound(data: any[]) {
+        return ({
+            type: 'bound',
+            min: data[0].value,
+            max: data[1].value ?? data[0].value,
+        });
+    }
+    function constructWildcard(data: any[]) {
+        return {
+            type: 'wildcard',
+            path: data[1],
+        };
+    }
+
 %}
 
 @lexer basicPromptLexer
-variant_prompt           -> variant_chunk:* {% id %}
+variant_prompt           -> (variant_chunk {% id %}):* {% id %}
 
-variant_chunk            -> (variants | wildcard | variant_literal_sequence) {% unwrap %}
+variant_chunk            -> (variants | wildcard | variant_literal_sequence)                  {% unwrap %}
+variants                 -> %lmoustache bound:? variants_list:?  %rmoustache                  {% wrapVariants %}
+variants_list            -> variant (%bar variant {% (data) => data[1][0] %}):*               {% flattenVariantsList %}
+variant                  -> weight:? variant_prompt                                           {% data => data[1] %}
 
-variants                 -> %lmoustache bound:? variants_list:?  %rmoustache {%
-    (data) => ({
-            bound: data[1] ?? DEFAULT_BOUND,
-            variants: data[2],
-            })
-%}
-variants_list            -> variant (%bar variant {% (data) => data[1][0] %}):* {%
-    data => [data[0][0], ...data[1]]
-%}
-variant                  -> weight:? variant_prompt {% data => data[1] %}
-weight                   -> %number | %integer {% tag('weight')  %}
-bound                    -> %integer (%dash %integer {% data => data[1] %}):? %dollar %dollar {% data => ({
-    min: data[0].value,
-    max: data[1].value ?? data[0].value
-}) %}
-wildcard                 -> %wildcard_enclosure %path %wildcard_enclosure {%
- (data) => ({
-    type: 'wildcard',
-    path: data[1],
-    })
-%}
-variant_literal_sequence -> (%variant_literal {% data => {
-    return data[0].value;
-} %}):+ {% unwrap %}
+weight                   -> %number | %integer                                                {% tag('weight')  %}
+bound                    -> %integer (%dash %integer {% data => data[1] %}):? %dollar %dollar {% constructBound %}
+
+wildcard                 -> %wildcard_enclosure %path %wildcard_enclosure                     {% constructWildcard %}
+variant_literal_sequence -> (%variant_literal {% constructLiteral %}):+                             {% unwrap %}
--- src/common/sdprompt.ts
+++ src/common/sdprompt.ts
@@ -17,12 +17,47 @@ declare var variant_literal: any;
     // eslint-disable
     // @ts-nocheck
     import {basicPromptLexer} from './sdprompt_lexer';
-    const tag = (key: string) => (data: any[]) => [key, ...data.flat()]; 
-    const unwrap = (data: any[]) => data[0][0];
     const DEFAULT_BOUND = {
         min: "1",
         max: "1",
     };
+    
+    const tag = (key: string) => (data: any[]) => [key, ...data.flat()]; 
+    const unwrap = (data: any[]) => data[0][0];
+    
+    function wrapVariants(data: any[]) {
+        return {
+            type: 'variants',
+            bound: data[1] ?? DEFAULT_BOUND,
+            variants: data[2],
+        };
+    }
+    function flattenVariantsList(data: any[]) {
+        return [data[0][0], ...data[1]];
+    }
+    function lexerValue(data: any[]) {
+        return data[0].value;
+    }
+    function constructLiteral(data: any[]) {
+        return {
+            type: 'literal',
+            value: data[0].value,
+        }
+    }
+    function constructBound(data: any[]) {
+        return ({
+            type: 'bound',
+            min: data[0].value,
+            max: data[1].value ?? data[0].value,
+        });
+    }
+    function constructWildcard(data: any[]) {
+        return {
+            type: 'wildcard',
+            path: data[1],
+        };
+    }
+
 
 interface NearleyToken {
   value: any;
@@ -55,7 +90,8 @@ const grammar: Grammar = {
   Lexer: basicPromptLexer,
   ParserRules: [
     {"name": "variant_prompt$ebnf$1", "symbols": []},
-    {"name": "variant_prompt$ebnf$1", "symbols": ["variant_prompt$ebnf$1", "variant_chunk"], "postprocess": (d) => d[0].concat([d[1]])},
+    {"name": "variant_prompt$ebnf$1$subexpression$1", "symbols": ["variant_chunk"], "postprocess": id},
+    {"name": "variant_prompt$ebnf$1", "symbols": ["variant_prompt$ebnf$1", "variant_prompt$ebnf$1$subexpression$1"], "postprocess": (d) => d[0].concat([d[1]])},
     {"name": "variant_prompt", "symbols": ["variant_prompt$ebnf$1"], "postprocess": id},
     {"name": "variant_chunk$subexpression$1", "symbols": ["variants"]},
     {"name": "variant_chunk$subexpression$1", "symbols": ["wildcard"]},
@@ -65,18 +101,11 @@ const grammar: Grammar = {
     {"name": "variants$ebnf$1", "symbols": [], "postprocess": () => null},
     {"name": "variants$ebnf$2", "symbols": ["variants_list"], "postprocess": id},
     {"name": "variants$ebnf$2", "symbols": [], "postprocess": () => null},
-    {"name": "variants", "symbols": [(basicPromptLexer.has("lmoustache") ? {type: "lmoustache"} : lmoustache), "variants$ebnf$1", "variants$ebnf$2", (basicPromptLexer.has("rmoustache") ? {type: "rmoustache"} : rmoustache)], "postprocess": 
-        (data) => ({
-                bound: data[1] ?? DEFAULT_BOUND,
-                variants: data[2],
-                })
-        },
+    {"name": "variants", "symbols": [(basicPromptLexer.has("lmoustache") ? {type: "lmoustache"} : lmoustache), "variants$ebnf$1", "variants$ebnf$2", (basicPromptLexer.has("rmoustache") ? {type: "rmoustache"} : rmoustache)], "postprocess": wrapVariants},
     {"name": "variants_list$ebnf$1", "symbols": []},
     {"name": "variants_list$ebnf$1$subexpression$1", "symbols": [(basicPromptLexer.has("bar") ? {type: "bar"} : bar), "variant"], "postprocess": (data) => data[1][0]},
     {"name": "variants_list$ebnf$1", "symbols": ["variants_list$ebnf$1", "variants_list$ebnf$1$subexpression$1"], "postprocess": (d) => d[0].concat([d[1]])},
-    {"name": "variants_list", "symbols": ["variant", "variants_list$ebnf$1"], "postprocess": 
-        data => [data[0][0], ...data[1]]
-        },
+    {"name": "variants_list", "symbols": ["variant", "variants_list$ebnf$1"], "postprocess": flattenVariantsList},
     {"name": "variant$ebnf$1", "symbols": ["weight"], "postprocess": id},
     {"name": "variant$ebnf$1", "symbols": [], "postprocess": () => null},
     {"name": "variant", "symbols": ["variant$ebnf$1", "variant_prompt"], "postprocess": data => data[1]},
@@ -85,23 +114,11 @@ const grammar: Grammar = {
     {"name": "bound$ebnf$1$subexpression$1", "symbols": [(basicPromptLexer.has("dash") ? {type: "dash"} : dash), (basicPromptLexer.has("integer") ? {type: "integer"} : integer)], "postprocess": data => data[1]},
     {"name": "bound$ebnf$1", "symbols": ["bound$ebnf$1$subexpression$1"], "postprocess": id},
     {"name": "bound$ebnf$1", "symbols": [], "postprocess": () => null},
-    {"name": "bound", "symbols": [(basicPromptLexer.has("integer") ? {type: "integer"} : integer), "bound$ebnf$1", (basicPromptLexer.has("dollar") ? {type: "dollar"} : dollar), (basicPromptLexer.has("dollar") ? {type: "dollar"} : dollar)], "postprocess":  data => ({
-            min: data[0].value,
-            max: data[1].value ?? data[0].value
-        }) },
-    {"name": "wildcard", "symbols": [(basicPromptLexer.has("wildcard_enclosure") ? {type: "wildcard_enclosure"} : wildcard_enclosure), (basicPromptLexer.has("path") ? {type: "path"} : path), (basicPromptLexer.has("wildcard_enclosure") ? {type: "wildcard_enclosure"} : wildcard_enclosure)], "postprocess": 
-        (data) => ({
-           type: 'wildcard',
-           path: data[1],
-           })
-        },
-    {"name": "variant_literal_sequence$ebnf$1$subexpression$1", "symbols": [(basicPromptLexer.has("variant_literal") ? {type: "variant_literal"} : variant_literal)], "postprocess":  data => {
-            return data[0].value;
-        } },
+    {"name": "bound", "symbols": [(basicPromptLexer.has("integer") ? {type: "integer"} : integer), "bound$ebnf$1", (basicPromptLexer.has("dollar") ? {type: "dollar"} : dollar), (basicPromptLexer.has("dollar") ? {type: "dollar"} : dollar)], "postprocess": constructBound},
+    {"name": "wildcard", "symbols": [(basicPromptLexer.has("wildcard_enclosure") ? {type: "wildcard_enclosure"} : wildcard_enclosure), (basicPromptLexer.has("path") ? {type: "path"} : path), (basicPromptLexer.has("wildcard_enclosure") ? {type: "wildcard_enclosure"} : wildcard_enclosure)], "postprocess": constructWildcard},
+    {"name": "variant_literal_sequence$ebnf$1$subexpression$1", "symbols": [(basicPromptLexer.has("variant_literal") ? {type: "variant_literal"} : variant_literal)], "postprocess": constructLiteral},
     {"name": "variant_literal_sequence$ebnf$1", "symbols": ["variant_literal_sequence$ebnf$1$subexpression$1"]},
-    {"name": "variant_literal_sequence$ebnf$1$subexpression$2", "symbols": [(basicPromptLexer.has("variant_literal") ? {type: "variant_literal"} : variant_literal)], "postprocess":  data => {
-            return data[0].value;
-        } },
+    {"name": "variant_literal_sequence$ebnf$1$subexpression$2", "symbols": [(basicPromptLexer.has("variant_literal") ? {type: "variant_literal"} : variant_literal)], "postprocess": constructLiteral},
     {"name": "variant_literal_sequence$ebnf$1", "symbols": ["variant_literal_sequence$ebnf$1", "variant_literal_sequence$ebnf$1$subexpression$2"], "postprocess": (d) => d[0].concat([d[1]])},
     {"name": "variant_literal_sequence", "symbols": ["variant_literal_sequence$ebnf$1"], "postprocess": unwrap}
   ],
