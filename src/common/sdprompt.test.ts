bdabb355a84f95258ff21294d1f92dbc39e7ecd5
Alexander Brown <DrJKL0424@gmail.com>
2023-08-09 17:09:51 -0700

    Remove logging from tests for now

--- src/common/sdprompt.test.ts
+++ src/common/sdprompt.test.ts
@@ -10,19 +10,19 @@ describe('parser', () => {
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     const prompt = `a picture of a dog wearing a fancy hat`;
     parser.feed(prompt);
-    console.log(JSON.stringify(parser.results));
+    // console.log(JSON.stringify(parser.results));
   });
 
   it('should lex a prompt with wildcards', () => {
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     const prompt = `a picture of a {dog|cat|frog} wearing a fancy hat`;
     parser.feed(prompt);
-    console.log(JSON.stringify(parser.results, null, 2));
+    // console.log(JSON.stringify(parser.results, null, 2));
   });
 
   it('should parse a long dynamic prompt', () => {
     const prompt = LANDSCAPE_VERY_DYNAMIC;
     parser.feed(prompt);
-    console.log(JSON.stringify(parser.results, null, 2));
+    // console.log(JSON.stringify(parser.results, null, 2));
   });
 });
