03875d507fc3d29f74d8284aa45f5d99617ea164
Alexander Brown <DrJKL0424@gmail.com>
2023-08-17 16:11:11 -0700

    YAGNI

--- src/common/saving/prompt_saving.ts
+++
@@ -1,6 +0,0 @@
-import { getSavedPrompts } from './localstorage';
-
-function saveExists(promptName: string): boolean {
-  const saved = getSavedPrompts();
-  return saved.some((prompt) => prompt.name === promptName);
-}
