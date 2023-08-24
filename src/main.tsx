f228122cbc0a8bba6a426e5549c56c6ebef6e3d9
Alexander Brown <DrJKL0424@gmail.com>
2023-08-24 11:45:35 -0700

    I hope this isn't necessary, but add Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { Button, ThemeProvider, createTheme } from '@mui/material';
 import { red } from '@mui/material/colors';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import { saveActivePrompt } from './common/saving/localstorage.ts';
 
 const theme = createTheme({
   palette: {
@@ -14,11 +16,43 @@ const theme = createTheme({
     },
   },
 });
+
+function fallbackRender({ error, resetErrorBoundary }: FallbackProps) {
+  const message =
+    error instanceof Error
+      ? error.message
+      : JSON.stringify(error, undefined, 2);
+  function resetActivePrompt() {
+    saveActivePrompt('');
+  }
+  return (
+    <div className="text-red-600">
+      <h1>Oh No! Something went wrong!</h1>
+      <main>
+        <pre className="w-auto ">{message}</pre>
+      </main>
+
+      <div>
+        <Button variant="outlined" onClick={resetActivePrompt}>
+          Reset Active Prompt?
+        </Button>
+      </div>
+      <div>
+        <Button variant="outlined" onClick={resetErrorBoundary}>
+          Retry?
+        </Button>
+      </div>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <ErrorBoundary fallbackRender={fallbackRender}>
+          <App />
+        </ErrorBoundary>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
