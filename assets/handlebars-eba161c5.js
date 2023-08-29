eb97c738b4b52746d330bc4d7ad97b3af38479d3
Alexander Brown <DrJKL0424@gmail.com>
2023-08-29 14:52:56 -0700

    Updates

--- assets/handlebars-eba161c5.js
+++ assets/handlebars-eba161c5.js
@@ -1,4 +1,4 @@
-import{m as i}from"./index-178c19ed.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./index-9ad80ca8.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.41.0(38e1e3d097f84e336c311d071a9ffb5191d4ffd1)
  * Released under the MIT license
--- assets/javascript-8e22124e.js
+++ assets/javascript-8e22124e.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-cece37fe.js";import"./index-178c19ed.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-e1ab107d.js";import"./index-9ad80ca8.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.41.0(38e1e3d097f84e336c311d071a9ffb5191d4ffd1)
  * Released under the MIT license
