ef4da1e647bd0f4695582b0275b6d707a54c6ee3
Alexander Brown <DrJKL0424@gmail.com>
2023-08-29 11:18:11 -0700

    Refactor: Extract immer reduce/produce utility

--- src/App.tsx
+++ src/App.tsx
@@ -37,79 +37,15 @@ import { SavedPromptDisplay } from './components/SavedPrompt';
 import { SizeUnit } from 'react-spaces/dist/core-types';
 import { editAttentionMonaco } from './common/tweaks/edit_attention';
 import { getPromptTokens, parsePrompt } from './common/parsing/app_parsing';
-import { Draft, current } from 'immer';
-import { ParseResult, Prompt } from './common/rendering/parsed_types';
-import { randomizeAllResults } from './common/random/randomize';
-import seedrandom, { PRNG } from 'seedrandom';
+import { ParseResult } from './common/rendering/parsed_types';
+import seedrandom from 'seedrandom';
+import {
+  ModifyPromptAction,
+  variantSelectionReducer,
+} from './common/prompt_modification';
 
 const minHeight =
   3 * parseInt(getComputedStyle(document.documentElement)?.fontSize);
-
-type ModifyPromptAction =
-  | { type: 'reset'; results: ParseResult }
-  | { type: 'randomize'; prng: PRNG }
-  | { type: 'choose-variant'; path: number[]; selection: number[] };
-
-function variantSelectionReducer(
-  draft: Draft<ParseResult>,
-  action: ModifyPromptAction,
-) {
-  switch (action.type) {
-    case 'reset':
-      return action.results;
-    case 'choose-variant':
-      return modifySelection(draft, action.path, action.selection);
-    case 'randomize':
-      randomizeAllResults(draft, action.prng);
-      return;
-  }
-  action satisfies never;
-  return draft;
-}
-
-function modifySelection(
-  draft: Draft<ParseResult>,
-  path: number[],
-  selection: number[],
-) {
-  const undraft = current(draft);
-  const firstParse = draft[0];
-  if (!firstParse) {
-    console.error(`Can't modify selection, bad Chunks provided: ${undraft}`);
-    return draft;
-  }
-  let chunkCursor: Draft<Prompt>[] = firstParse;
-  for (let i = 0; i < path.length - 2; i += 2) {
-    if (path[i] < 0 || path[i + 1] < 0) {
-      return;
-    }
-    const chunk = chunkCursor[path[i]];
-    const nextVariants = chunk[path[i + 1]];
-    if (nextVariants?.type !== 'variants') {
-      console.error(`Found non-variable sub-path when trying to update
-      ${chunk} in
-      ${draft} with path
-      ${path}`);
-
-      return draft;
-    }
-    chunkCursor = [...nextVariants.variants];
-  }
-  try {
-    const leafHopefully =
-      chunkCursor[path[path.length - 2]][path[path.length - 1]];
-    if (leafHopefully?.type === 'variants') {
-      leafHopefully.selections = selection;
-      return;
-    }
-    console.error(
-      'Something has gone horribly awry',
-      JSON.parse(JSON.stringify(leafHopefully)),
-    );
-  } catch (err: unknown) {
-    console.error(err);
-  }
-}
 
 function App() {
   const [promptText, setPromptText] = useState<string>(getActivePromptLocal());
