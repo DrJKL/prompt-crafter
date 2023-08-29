13405110c37d3aedd19be10917a32114011d1870
Alexander Brown <DrJKL0424@gmail.com>
2023-08-29 11:42:49 -0700

    Have to do it in Vite too...

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pc-random': path.resolve(__dirname, './src/common/random'),
+      '@rendering': path.resolve(__dirname, './src/common/rendering'),
+    },
+  },
   plugins: [
     react(),
     monacoEditorPlugin.default({ languageWorkers: [], customWorkers: [] }),
