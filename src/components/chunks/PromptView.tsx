80bf3e1b79607fd5d09d48d8b06642e1d12ef0db
Alexander Brown <DrJKL0424@gmail.com>
2023-08-31 14:51:12 -0700

    No longer need to forward a ref or animate the span

--- src/components/chunks/PromptView.tsx
+++ src/components/chunks/PromptView.tsx
@@ -1,34 +1,33 @@
-import { animated } from '@react-spring/web';
 import { Prompt } from '@rendering/parsed_types';
 import { KeyPath } from '@rendering/rendering_utils';
-import { ForwardedRef, forwardRef } from 'react';
 import { ChunkView } from './ChunkView';
 
 interface PromptProps extends KeyPath {
   prompt: Prompt;
 }
 
-export const PromptView = forwardRef<HTMLSpanElement, PromptProps>(
-  function PromptView(
-    { prompt, path, updateSelection, fancy, dense }: PromptProps,
-    ref: ForwardedRef<HTMLSpanElement>,
-  ) {
-    return (
-      <animated.span className="inline-block" ref={ref}>
-        {prompt?.map((c, idx) => {
-          const newPath = [...path, idx];
-          return (
-            <ChunkView
-              key={idx}
-              chunk={c}
-              path={newPath}
-              updateSelection={updateSelection}
-              fancy={fancy}
-              dense={dense}
-            />
-          );
-        })}{' '}
-      </animated.span>
-    );
-  },
-);
+export function PromptView({
+  prompt,
+  path,
+  updateSelection,
+  fancy,
+  dense,
+}: PromptProps) {
+  return (
+    <>
+      {prompt?.map((c, idx) => {
+        const newPath = [...path, idx];
+        return (
+          <ChunkView
+            key={idx}
+            chunk={c}
+            path={newPath}
+            updateSelection={updateSelection}
+            fancy={fancy}
+            dense={dense}
+          />
+        );
+      })}{' '}
+    </>
+  );
+}
