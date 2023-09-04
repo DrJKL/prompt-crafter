91dfb60611917a808a5acbcc6facf90e60c5ba7c
Alexander Brown <DrJKL0424@gmail.com>
2023-09-03 17:49:21 -0700

    Updates

--- assets/freemarker2-77327771.js
+++ assets/freemarker2-77327771.js
@@ -1,4 +1,4 @@
-import{m as F}from"./index-9afa9ada.js";/*!-----------------------------------------------------------------------------
+import{m as F}from"./index-9e6a4839.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.41.0(38e1e3d097f84e336c311d071a9ffb5191d4ffd1)
  * Released under the MIT license
