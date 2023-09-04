16785e381cd288199c0c1d4030f9a57a25adde30
Alexander Brown <DrJKL0424@gmail.com>
2023-09-04 00:10:31 -0700

    Still not sure how to best represent variables.

--- src/components/chunks/VariableView.tsx
+++ src/components/chunks/VariableView.tsx
@@ -1,13 +1,14 @@
 import { Variable, VariableFlavor } from '@rendering/parsed_types';
 import { KeyPath, pathToString } from '@rendering/rendering_utils';
 import { PromptView } from './PromptView';
+import { Badge, Tooltip } from '@mui/material';
 
 interface VariableProps extends KeyPath {
   variable: Variable;
 }
 
 const FLAVOR_TO_SYMBOL: Record<VariableFlavor, string> = {
-  access: '~ ',
+  access: '',
   assignment: '= ',
   assignmentImmediate: '=!',
   unknown: '¿¿',
@@ -20,23 +21,25 @@ export function VariableView({
   ...props
 }: VariableProps) {
   return (
-    <span
-      title={pathToString('variable', path)}
-      className={`${
-        fancy
-          ? 'border-rose-600 border-opacity-50 border-2 rounded-md flex-[0_1_fit-content] inline-flex'
-          : ''
-      } text-violet-300 font-bold p-1 items-end`}>
-      {variable.name}
-      {FLAVOR_TO_SYMBOL[variable.flavor]}
-      {variable.value && (
-        <PromptView
-          path={[...path, 0]}
-          prompt={variable.value[0]}
-          fancy={fancy}
-          {...props}
-        />
-      )}
-    </span>
+    <Tooltip
+      title={`variable: ${variable.name}${FLAVOR_TO_SYMBOL[variable.flavor]}`}
+      color="primary">
+      <span
+        title={pathToString('variable', path)}
+        className={`${
+          fancy
+            ? '' //'border-rose-600 border-opacity-50 border-2 rounded-md flex-[0_1_fit-content] inline-flex'
+            : ''
+        } text-violet-300 font-bold p-1 items-end`}>
+        {variable.value && (
+          <PromptView
+            path={[...path, 0]}
+            prompt={variable.value[0]}
+            fancy={fancy}
+            {...props}
+          />
+        )}
+      </span>
+    </Tooltip>
   );
 }
